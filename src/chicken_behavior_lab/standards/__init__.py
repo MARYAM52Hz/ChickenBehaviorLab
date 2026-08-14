bcbd43b8cb0e3ad4d8b4254dcc8917bc17c1cb7d
maryam abbasi <m.abbasi.6549@gmail.com>
2026-08-14 23:49:47 +0330

    Create __init__.py

---
+++ src/chicken_behavior_lab/standards/__init__.py
@@ -0,0 +1,19 @@
+"""
+ChickenBehaviorLab Standards
+============================
+
+CBAS and CBO reference implementations.
+"""
+
+from chicken_behavior_lab.standards.cbas import (
+    CBAS_VERSION,
+)
+
+from chicken_behavior_lab.standards.cbo import (
+    CBO_VERSION,
+)
+
+__all__ = [
+    "CBAS_VERSION",
+    "CBO_VERSION",
+]
