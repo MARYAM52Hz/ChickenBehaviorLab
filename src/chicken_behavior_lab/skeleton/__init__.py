36dbf1d07a3dd1dd0bbd994ed367436256a1ca09
maryam abbasi <m.abbasi.6549@gmail.com>
2026-08-21 14:39:23 +0330

    Create __init__.py

---
+++ src/chicken_behavior_lab/skeleton/__init__.py
@@ -0,0 +1,18 @@
+"""
+ChickenBehaviorLab Skeleton Module
+==================================
+"""
+
+from chicken_behavior_lab.skeleton.base import (
+    CBAS_SKELETON_CONNECTIONS,
+)
+
+from chicken_behavior_lab.skeleton.builder import (
+    SkeletonBuilder,
+)
+
+
+__all__ = [
+    "CBAS_SKELETON_CONNECTIONS",
+    "SkeletonBuilder",
+]
