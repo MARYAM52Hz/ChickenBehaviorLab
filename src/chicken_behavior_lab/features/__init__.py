6efc14ed688705fdf5e47ca15d29f4e20e12b0d3
maryam abbasi <m.abbasi.6549@gmail.com>
2026-08-25 22:18:17 +0330

    Create __init__.py

---
+++ src/chicken_behavior_lab/features/__init__.py
@@ -0,0 +1,15 @@
+"""
+ChickenBehaviorLab Feature Extraction
+======================================
+"""
+
+from chicken_behavior_lab.features.position import (
+    PositionFeatures,
+    PositionFeatureExtractor,
+)
+
+
+__all__ = [
+    "PositionFeatures",
+    "PositionFeatureExtractor",
+]
