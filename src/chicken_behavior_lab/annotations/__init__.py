43d7d3d36221a80a0b2a7c3a6931d07da0941c91
maryam abbasi <m.abbasi.6549@gmail.com>
2026-08-30 12:53:23 +0330

    Create __init__.py

---
+++ src/chicken_behavior_lab/annotations/__init__.py
@@ -0,0 +1,19 @@
+"""
+ChickenBehaviorLab Annotation Module
+====================================
+"""
+
+from chicken_behavior_lab.annotations.labels import (
+    BehaviorLabel,
+    BehaviorLabelEncoder,
+    DEFAULT_BEHAVIOR_LABELS,
+    DEFAULT_BEHAVIOR_ENCODER,
+)
+
+
+__all__ = [
+    "BehaviorLabel",
+    "BehaviorLabelEncoder",
+    "DEFAULT_BEHAVIOR_LABELS",
+    "DEFAULT_BEHAVIOR_ENCODER",
+]
