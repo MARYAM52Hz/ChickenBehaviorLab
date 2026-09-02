e49b7394079d3741e16204dddfee61035cc1e9e6
maryam abbasi <m.abbasi.6549@gmail.com>
2026-09-02 22:07:41 +0330

    Update __init__.py

--- src/chicken_behavior_lab/annotations/__init__.py
+++ src/chicken_behavior_lab/annotations/__init__.py
@@ -10,10 +10,22 @@ from chicken_behavior_lab.annotations.labels import (
     DEFAULT_BEHAVIOR_ENCODER,
 )
 
+from chicken_behavior_lab.annotations.schema import (
+    BehaviorAnnotation,
+    AnnotationSet,
+)
+
+from chicken_behavior_lab.annotations.loader import (
+    AnnotationLoader,
+)
+
 
 __all__ = [
     "BehaviorLabel",
     "BehaviorLabelEncoder",
     "DEFAULT_BEHAVIOR_LABELS",
     "DEFAULT_BEHAVIOR_ENCODER",
+    "BehaviorAnnotation",
+    "AnnotationSet",
+    "AnnotationLoader",
 ]
