afd491b1da3d92bd9b9b1eb4e92fc141a656b78d
maryam abbasi <m.abbasi.6549@gmail.com>
2026-09-06 22:02:55 +0330

    Update __init__.py

--- src/chicken_behavior_lab/dataset/__init__.py
+++ src/chicken_behavior_lab/dataset/__init__.py
@@ -1,10 +1,21 @@
-from chicken_behavior_lab.dataset.sample import GraphSample
-from chicken_behavior_lab.dataset.graph_dataset import GraphDataset
+from chicken_behavior_lab.dataset.sample import (
+    GraphSample,
+)
+
+from chicken_behavior_lab.dataset.graph_dataset import (
+    GraphDataset,
+)
+
 from chicken_behavior_lab.dataset.builder import (
     DatasetBuilder,
     FeatureSequenceProvider,
     GraphBuilderProtocol,
 )
+
+from chicken_behavior_lab.dataset.pyg_dataset import (
+    PyGGraphDataset,
+)
+
 
 __all__ = [
     "GraphSample",
@@ -12,4 +23,5 @@ __all__ = [
     "DatasetBuilder",
     "FeatureSequenceProvider",
     "GraphBuilderProtocol",
+    "PyGGraphDataset",
 ]
