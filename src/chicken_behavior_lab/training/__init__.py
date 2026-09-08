a12d9bc0815990bca5700ca3eaab660f186569ec
maryam abbasi <m.abbasi.6549@gmail.com>
2026-09-08 19:33:51 +0330

    Update __init__.py

--- src/chicken_behavior_lab/training/__init__.py
+++ src/chicken_behavior_lab/training/__init__.py
@@ -1,8 +1,31 @@
+from chicken_behavior_lab.training.config import (
+    TrainingConfig,
+)
+
 from chicken_behavior_lab.training.losses import (
     BehaviorClassificationLoss,
 )
 
+from chicken_behavior_lab.training.metrics import (
+    ClassificationMetrics,
+    build_confusion_matrix,
+    compute_classification_metrics,
+)
+
+from chicken_behavior_lab.training.trainer import (
+    EpochResult,
+    Trainer,
+    TrainingHistory,
+)
+
 
 __all__ = [
+    "TrainingConfig",
     "BehaviorClassificationLoss",
+    "ClassificationMetrics",
+    "build_confusion_matrix",
+    "compute_classification_metrics",
+    "EpochResult",
+    "Trainer",
+    "TrainingHistory",
 ]
