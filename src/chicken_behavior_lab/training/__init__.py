eb941d38c7df88deee745d10e31b31eeb79505c3
maryam abbasi <m.abbasi.6549@gmail.com>
2026-09-12 20:04:28 +0330

    Update __init__.py

--- src/chicken_behavior_lab/training/__init__.py
+++ src/chicken_behavior_lab/training/__init__.py
@@ -12,6 +12,11 @@ from chicken_behavior_lab.training.metrics import (
     format_classification_report,
 )
 
+from chicken_behavior_lab.training.checkpoint import (
+    save_checkpoint,
+    load_checkpoint,
+)
+
 from chicken_behavior_lab.training.trainer import (
     Trainer,
     TrainingHistory,
@@ -29,6 +34,8 @@ __all__ = [
     "ClassificationMetrics",
     "compute_classification_metrics",
     "format_classification_report",
+    "save_checkpoint",
+    "load_checkpoint",
     "Trainer",
     "TrainingHistory",
     "EvaluationResult",
