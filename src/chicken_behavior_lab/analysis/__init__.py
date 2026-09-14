94eaa031c03efa17d474fe1a858ffd87991e6d3a
maryam abbasi <m.abbasi.6549@gmail.com>
2026-09-14 14:25:46 +0330

    Create __init__.py

---
+++ src/chicken_behavior_lab/analysis/__init__.py
@@ -0,0 +1,12 @@
+from chicken_behavior_lab.analysis.training_curves import (
+    TrainingHistoryAnalyzer,
+)
+
+from chicken_behavior_lab.analysis.confusion_matrix import (
+    ConfusionMatrixAnalyzer,
+)
+
+__all__ = [
+    "TrainingHistoryAnalyzer",
+    "ConfusionMatrixAnalyzer",
+]
