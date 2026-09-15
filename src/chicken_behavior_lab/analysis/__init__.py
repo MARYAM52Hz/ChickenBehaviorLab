af86ee96289737f6963ca21534170873838548fe
maryam abbasi <m.abbasi.6549@gmail.com>
2026-09-15 17:13:44 +0330

    Update __init__.py

--- src/chicken_behavior_lab/analysis/__init__.py
+++ src/chicken_behavior_lab/analysis/__init__.py
@@ -6,7 +6,14 @@ from chicken_behavior_lab.analysis.confusion_matrix import (
     ConfusionMatrixAnalyzer,
 )
 
+from chicken_behavior_lab.analysis.error_analysis import (
+    PredictionRecord,
+    PredictionErrorAnalyzer,
+)
+
 __all__ = [
     "TrainingHistoryAnalyzer",
     "ConfusionMatrixAnalyzer",
+    "PredictionRecord",
+    "PredictionErrorAnalyzer",
 ]
