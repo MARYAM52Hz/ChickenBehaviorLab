61d39660f3dba88ffe5e49d44bb257c051953500
maryam abbasi <m.abbasi.6549@gmail.com>
2026-09-22 16:17:19 +0330

    Update __init__.py

--- src/chicken_behavior_lab/analysis/__init__.py
+++ src/chicken_behavior_lab/analysis/__init__.py
@@ -1,32 +1,25 @@
-from chicken_behavior_lab.analysis.training_curves import (
+from chicken_behavior_lab.analysis.training_history import (
     TrainingHistoryAnalyzer,
 )
-
 from chicken_behavior_lab.analysis.confusion_matrix import (
     ConfusionMatrixAnalyzer,
 )
-
-from chicken_behavior_lab.analysis.error_analysis import (
+from chicken_behavior_lab.analysis.prediction_records import (
     PredictionRecord,
+)
+from chicken_behavior_lab.analysis.error_analysis import (
     PredictionErrorAnalyzer,
 )
-
-from chicken_behavior_lab.analysis.feature_diagnostics import (
-    FeatureDiagnosticRecord,
-    FeatureDiagnosticAnalyzer,
+from chicken_behavior_lab.analysis.temporal_error_analysis import (
+    TemporalPredictionError,
+    TemporalPredictionErrorAnalyzer,
 )
-
-from chicken_behavior_lab.analysis.feature_extractor import (
-    GraphFeatureDiagnosticExtractor,
-)
-
 
 __all__ = [
     "TrainingHistoryAnalyzer",
     "ConfusionMatrixAnalyzer",
     "PredictionRecord",
     "PredictionErrorAnalyzer",
-    "FeatureDiagnosticRecord",
-    "FeatureDiagnosticAnalyzer",
-    "GraphFeatureDiagnosticExtractor",
+    "TemporalPredictionError",
+    "TemporalPredictionErrorAnalyzer",
 ]
